d77e1e33777f3470780647c1fbb720ff614d3187
boogie <76754631+BoogieHasFun@users.noreply.github.com>
2023-03-26 14:18:13 -0400

    add template

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace learning_gtag_modcreation
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.boogiehasfun.gorillatag.learning-gtag-modcreation";
+		public const string Name = "learning_gtag_modcreation";
+		public const string Version = "1.0.0";
+	}
+}
